fa40d6cc490c7efa79cedb4a50b655f50d76d757
Thor Anker Kvisgård Lange <tal@netic.dk>
2023-03-08 11:29:45 +0100

    fix: Reading all body allow for connection reuse

--- httpcache.go
+++ httpcache.go
@@ -196,14 +196,16 @@ func (t *Transport) RoundTrip(req *http.Request) (resp *http.Response, err error
 			for _, header := range endToEndHeaders {
 				cachedResp.Header[header] = resp.Header[header]
 			}
-			resp.Body.Close()
+			_, _ = io.Copy(io.Discard, resp.Body)
+			_ = resp.Body.Close()
 			resp = cachedResp
 		} else if (err != nil || (cachedResp != nil && resp.StatusCode >= 500)) &&
 			req.Method == "GET" && canStaleOnError(cachedResp.Header, req.Header) {
 			// In case of transport failure and stale-if-error activated, returns cached content
 			// when available
 			if resp != nil && resp.Body != nil {
-				resp.Body.Close()
+				_, _ = io.Copy(io.Discard, resp.Body)
+				_ = resp.Body.Close()
 			}
 			return cachedResp, nil
 		} else {
